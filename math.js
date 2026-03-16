a004458abbc3e698b8085fc43800106d095c6af8
deepatharun-s <deepatharun.s@mitrahsoft.in>
2026-03-16 14:01:21 +0530

    define functions

---
+++ math.js
@@ -0,0 +1,15 @@
+function add(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+}
+
+function multiply(a,b){
+    return a*b
+}
+
+console.log(add(4,4))
+console.log(subtract(10,5))
+console.log(multiply(4,4));
