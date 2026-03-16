5c6d6caad34236ae8230f8bf82f325cc64f3dfc7
deepatharun-s <deepatharun.s@mitrahsoft.in>
2026-03-16 15:09:41 +0530

    invoke functions

--- math.js
+++ math.js
@@ -13,3 +13,7 @@ function multiply(a,b){
 console.log(add(4,4))
 console.log(subtract(10,5))
 console.log(multiply(4,4));
+
+console.log(add(4,4))
+console.log(subtract(10-5));
+console.log(multiply(4*4));
