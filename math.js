d90b6e88b189b0cf5313e9fdcd8d4e178b6da073
deepatharun-s <deepatharun.s@mitrahsoft.in>
2026-03-17 11:42:08 +0530

    feat: added divide function

--- math.js
+++ math.js
@@ -6,12 +6,18 @@ function subtract(a,b){
     return a-b
 }
 
-function multiply(a,b){
-    return a*b
+function multiply(x,y){
+    return x*y
+}
+
+
+function divide(a,b){
+    return a/b
 }
 
 console.log(add(4,4))
 console.log(subtract(10,5))
 console.log(multiply(4,4));
+console.log(divide(10,5))
 
 
