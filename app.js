99e698333928ae325514dc65de617c5a330dcf76
deepatharun-s <deepatharun.s@mitrahsoft.in>
2026-03-20 13:40:59 +0530

    second line modified

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 console.log("Welcome to MitrahSoft im from another branch im deepatharun and im kiran kumar from cbe we are TL of the CBE branch");
 
-console.log("Today we learnt about git Merge");
+console.log("Today we learnt about git Merge and learnt how to handle conflict");
