409c6243542f1d2b07842e9be5a745c596710c6b
kirankumar0714 <kirankumar.s@mitrahsoft.in>
2026-03-20 13:23:14 +0530

    app.js is changed

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-console.log("Welcome to MitrahSoft im from another branch");
+console.log("Welcome to MitrahSoft im from another branch im kiran kumar from cbe");
 
 console.log("Today we learnt about git Merge");
