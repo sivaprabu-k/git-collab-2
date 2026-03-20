309324262953d9e8a4486b8894e7e843e3d65ef8
deepatharun-s <deepatharun.s@mitrahsoft.in>
2026-03-20 13:21:30 +0530

    second modified

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
-console.log("Welcome to MitrahSoft im from another branch");
+console.log("Welcome to MitrahSoft im from another branch im deepatharun");
 
 console.log("Today we learnt about git Merge");
